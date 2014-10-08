0615ed794cbb1612ea7e07ee9210494ad701d681
Hanna Holler Kamperud <hanna.kamperud@gmail.com>
2014-10-08 22:28:06 +0200

    New picture, more lightweight

--- ullApp/gamehandler.cpp
+++ ullApp/gamehandler.cpp
@@ -1,11 +1,14 @@
 #include "gamehandler.h"
+#include <QDebug>
+
 
 GameHandler::GameHandler(QObject *parent) :
     QObject(parent){}
 
 bool GameHandler::isNerve(int x, int y){
-//    if(x==y)
-//        return true;
+    qDebug()<<"x: "<<x<<" y: "<<y<<"\n";
+    // We should use http://lodev.org/lodepng/ for the image checking
+    // the x and y are the actual picture coordinates. no need to translate screen size and whatnot
     return true;
 }
 
