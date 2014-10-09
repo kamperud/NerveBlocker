d72a7258e07599edaa8b7ed9c53257ada1b4aab0
Solveig Hellan <solvehel@stud.ntnu.no>
2014-10-09 13:27:42 +0200

    isNerve functionality

--- ullApp/gamehandler.cpp
+++ ullApp/gamehandler.cpp
@@ -1,5 +1,6 @@
 #include "gamehandler.h"
 #include <QDebug>
+#include <QImage>
 
 
 GameHandler::GameHandler(QObject *parent) :
@@ -9,6 +10,13 @@ bool GameHandler::isNerve(int x, int y){
     qDebug()<<"x: "<<x<<" y: "<<y<<"\n";
     // We should use http://lodev.org/lodepng/ for the image checking
     // the x and y are the actual picture coordinates. no need to translate screen size and whatnot
-    return true;
-}
 
+    QImage *img = new QImage();
+    QString fileName = "gameImages/1_map.png";
+    bool isLoaded = img->load(fileName);
+
+    if (img->pixel(x,y) == qRgb(0,255,255))
+        return true;
+
+    return false;
+}
