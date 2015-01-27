adb160a88bb1fbc832856642b4384780c1d9ed78
Solveig Hellan <solvehel@stud.ntnu.no>
2015-01-27 12:12:33 +0100

    fb ting ios

---
+++ multimedia.h
@@ -0,0 +1,17 @@
+#ifndef MULTIMEDIA_H
+#define MULTIMEDIA_H
+
+#include <QMediaPlayer>
+#include <QMediaPlayer>
+
+class Multimedia
+{
+public:
+    Multimedia();
+    ~Multimedia();
+
+private:
+    QMediaPlayer player;
+};
+
+#endif // MULTIMEDIA_H
